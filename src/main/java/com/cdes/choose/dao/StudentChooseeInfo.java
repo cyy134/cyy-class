52efe63b8697b12d9a725a65964b2b3bdb445d20
cyy <2894949169@qq.com>
2020-03-09 20:35:37 +0800

    测试

--- src/main/java/com/cdes/choose/dao/StudentChooseeInfo.java
+++ src/main/java/com/cdes/choose/dao/StudentChooseeInfo.java
@@ -14,8 +14,8 @@ public interface StudentChooseeInfo {
     @Select("select * from studentchooseinfo")
     List<StudentChooseInfo> getstdenChooseInfos();
 
-    @Select("select * from studentchooseinfo where id = #{id}")
-    StudentChooseInfo getOneStudentChooseInfo(int id);
+//    @Select("select * from studentchooseinfo where id = #{id}")
+//    StudentChooseInfo getOneStudentChooseInfo(int id);
 
 //    @Select("select * from studentchooseinfo where stuId = #{id}")
 //    StudentChooseInfo getOnStudentChooseInfoByStuId(int id);
