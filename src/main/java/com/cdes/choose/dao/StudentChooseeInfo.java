a949985e551164f100fb32e0c07694541c846a64
cyy <2894949169@qq.com>
2020-03-09 20:31:46 +0800

    修改

--- src/main/java/com/cdes/choose/dao/StudentChooseeInfo.java
+++ src/main/java/com/cdes/choose/dao/StudentChooseeInfo.java
@@ -17,7 +17,7 @@ public interface StudentChooseeInfo {
     @Select("select * from studentchooseinfo where id = #{id}")
     StudentChooseInfo getOneStudentChooseInfo(int id);
 
-    @Select("select * from studentchooseinfo where stuId = #{id}")
-    StudentChooseInfo getOnStudentChooseInfoByStuId(int id);
+//    @Select("select * from studentchooseinfo where stuId = #{id}")
+//    StudentChooseInfo getOnStudentChooseInfoByStuId(int id);
 
 }
